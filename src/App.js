9ec4e0a9c5e5f1428ad5680bb9b755bc69e2f531
Your Name <you@example.com>
2019-08-30 13:37:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import Home from './home';
+import Login from './login';
+
+class App extends Component{
+  render() {
+    return(
+        <Router>
+          <div>
+            <h2>
+              Welcome to React Router
+            </h2>
+            <ul>
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/login'}>Login</Link></li>
+            </ul>
+            <hr />
+            <switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+            </switch>
+          </div>
+        </Router>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <h2>Welcome to Github My Account</h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
