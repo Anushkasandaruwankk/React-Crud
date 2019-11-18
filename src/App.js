d6dd8722614ad0fd0e1174e502b5d68dcd095a72
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-11-18 05:44:59 +0530

    z

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@
 
 
 
-
+/
+//             <ul>
+//               <li><Link to={'/'}>Home</Link></li>
+//               <li><Link to={'/login'}>Login</Link></li>
+//             </ul>
 //             <hr />
 //             <switch>
 //               <Route exact path='/' component={Home} />
