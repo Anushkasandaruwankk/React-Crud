d9c1005e7884cde2b98b51aa8a968dd202ddc212
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-11-19 05:51:19 +0530

    z

--- src/App.js
+++ src/App.js
@@ -30,5 +30,4 @@
 // export default App;
 import React, {Component} from 'react';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
-import Home from './home';
-import Login from './z'
+impz
