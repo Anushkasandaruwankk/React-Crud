eca2b3b0abf08d91ce71208923bef2e152771097
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-12-04 05:56:48 +0530

    c

--- src/App.js
+++ src/App.js
@@ -11,10 +11,5 @@ class App extends Component{
               Welcome to React Router
             </h2>
             <ul>
-              <li><Link to={'/'}>Home</Link></li>
-              <li><Link to={'/login'}>Login</Link></li>
-            </ul>
-            <hr />
-            <>
 
 
