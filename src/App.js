11d6823471f9023adb6b306bde247fa6070b7f28
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-12-04 05:55:22 +0530

    z

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './login';
 class App extends Component{
   render() {
     return(
-        <Route>
+        <Router>
           <div>
             <h2>
               Welcome to React Router
@@ -20,4 +20,4 @@ class App extends Component{
               <Route exact path='/login' component={Login} />
             </switch>
           </div>
-
+        </Router
