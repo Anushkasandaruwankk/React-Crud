f60097ee7338f477451cb87c3886fba623b1c653
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-12-05 06:00:00 +0530

    z

--- src/App.js
+++ src/App.js
@@ -15,5 +15,7 @@ class App extends Component{
               <li><Link to={'/login'}>Login</Link></li>
             </ul>
             <hr />
+            <>
+              <Route exact path='/' component={Home} />
 
 
