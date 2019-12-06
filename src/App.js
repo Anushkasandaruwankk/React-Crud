703181561c2e3afbe0b975223454b63c5e914775
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2019-12-06 06:04:39 +0530

    z

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import Home from './home';
 import Login from './login';
-
 class App extends Component{
+  render() {
+    return(
 
