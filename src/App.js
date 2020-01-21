f4781d0aa03086ac34e799329ae2e96c54180a5c
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-21 06:17:54 +0530

    z

--- src/App.js
+++ src/App.js
@@ -5,11 +5,4 @@ import Login from './login';
 class App extends Component{
   render() {
     return(
-        <Router>
-          <div>
-            <h2>
-              Welcome to React Router
-            </h2>
-            <ul>
-              <li><Link to={'/'}>Home</Link></li>
-              
+    
