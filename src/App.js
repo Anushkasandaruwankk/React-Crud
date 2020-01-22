f34a0d4784570bf23410ac6f080a31483086aa3a
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-01-22 06:20:29 +0530

    z

--- src/App.js
+++ src/App.js
@@ -12,4 +12,5 @@ class App extends Component{
             </h2>
             <ul>
               <li><Link to={'/'}>Home</Link></li>
-              
+              <li><Link to={'/login'}>Login</Link></li>
+
